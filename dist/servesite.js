36e2548028957feaf3ec56696180e13fa79d2eb1
William (Wgyt) Horning <github@wgyt.tk>
2021-03-07 17:20:47 +0000

    COMMENTS YAY // HI

--- dist/servesite.js
+++ dist/servesite.js
@@ -1,9 +1,9 @@
 exports.main = function(host, res, req, error, modules, version, eta) {
-  if (modules.website.includes(host) === true) {
+  if (modules.website.includes(host) === true) { // if the site is in modulesjson 
     module = modules.websiteData[host];
     Sitemodule = require(`${dirname}/modules/${module}/index.js`);
-    Sitemodule(host, res, req, error, version, eta);
-  } else {
+    Sitemodule(host, res, req, error, version, eta); // serve the site
+  } else { // otherwise throw error
     error(
       host,
       res,

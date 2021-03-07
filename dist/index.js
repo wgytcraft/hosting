687b43a7a6873ebf311b9e69c1072a57ac251da0
William (Wgyt) Horning <github@wgyt.tk>
2021-03-07 17:45:10 +0000

    ejs is the way to go, eta is broken

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@ exports.main = function (modules,dirname,port) {
 	const servesite = require("./servesite.js").main // this is the function that uses the modules to serve the site
 	const fs = require("fs-extra"); // deal with files
 	const http = require("http"); // http server
-	const eta = require("eta"); // templating engine
+	const ejs = require("ejs"); // templating engine
 	var clone = require("git-clone-sync"); // clone from git sources
 	const version = "1.0.3"; // verison number
 	importmodules(modules,dirname,clone); // import the modules
@@ -11,6 +11,6 @@ exports.main = function (modules,dirname,port) {
 	http.createServer((req, res) => { // set up the server
 			host = req.headers.host; // this is the host
 			res.setHeader("X-Powered-By","nodejs@wgytcraft/hosting")
-			servesite(host, res, req, error, modules, version, eta); // serve the site
+			servesite(host, res, req, error, modules, version, ejs); // serve the site
 	}).listen(port); // listen on port
 }

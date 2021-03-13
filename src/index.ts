e802965609057300451c45d95c926da2aaec08cb
William (Wgyt) Horning <williamhorning@wgyt.tk>
2021-03-13 16:01:10 -0500

    Co-authored-by: thecoder876 <thecoder876@users.noreply.github.com> and grahamsh-llk <grahamsh-llk@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-exports.main = function (modules,dirname,port) {
+exports.main = function (modules: any, dirname: string, port: number) {
 	const importmodules = require("./importmodules.js").main // this is the function that imports the modules
 	const servesite = require("./servesite.js").main // this is the function that uses the modules to serve the site
 	const fs = require("fs-extra"); // deal with files
@@ -6,11 +6,11 @@ exports.main = function (modules,dirname,port) {
 	const ejs = require("./ejs/ejs.js"); // templating engine
 	var clone = require("git-clone-sync"); // clone from git sources
 	const version = "1.1.0"; // verison number
-	importmodules(modules,dirname,clone); // import the modules
+	importmodules(modules, dirname, clone); // import the modules
 	const error = require(`${dirname}/modules/${modules.errorHandler.module}/index.js`); // get the error handler
 	http.createServer((req, res) => { // set up the server
-			host = req.headers.host; // this is the host
-			res.setHeader("X-Powered-By","nodejs@wgytcraft/hosting")
-			servesite(host, res, req, error, modules, version, ejs); // serve the site
+		const host = req.headers.host; // this is the host
+		res.setHeader("X-Powered-By", "nodejs@wgytcraft/hosting")
+		servesite(host, res, req, error, modules, version, ejs); // serve the site
 	}).listen(port); // listen on port
 }

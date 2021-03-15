cd6d9bc59069ddf8039ae6ad5bc94ed5e8694215
wgyt <wgyt@users.noreply.github.com>
2021-03-15 19:49:32 +0000

    style: format code

--- src/ncp/ncp.js
+++ src/ncp/ncp.js
@@ -1,10 +1,10 @@
-var fs = require('fs'),
-    path = require('path');
+var fs = require("fs"),
+  path = require("path");
 
 module.exports = ncp;
 ncp.ncp = ncp;
 
-function ncp (source, dest, options, callback) {
+function ncp(source, dest, options, callback) {
   var cback = callback;
 
   if (!callback) {
@@ -13,24 +13,24 @@ function ncp (source, dest, options, callback) {
   }
 
   var basePath = process.cwd(),
-      currentPath = path.resolve(basePath, source),
-      targetPath = path.resolve(basePath, dest),
-      filter = options.filter,
-      rename = options.rename,
-      transform = options.transform,
-      clobber = options.clobber !== false,
-      modified = options.modified,
-      dereference = options.dereference,
-      errs = null,
-      started = 0,
-      finished = 0,
-      running = 0,
-      limit = options.limit || ncp.limit || 16;
+    currentPath = path.resolve(basePath, source),
+    targetPath = path.resolve(basePath, dest),
+    filter = options.filter,
+    rename = options.rename,
+    transform = options.transform,
+    clobber = options.clobber !== false,
+    modified = options.modified,
+    dereference = options.dereference,
+    errs = null,
+    started = 0,
+    finished = 0,
+    running = 0,
+    limit = options.limit || ncp.limit || 16;
 
-  limit = (limit < 1) ? 1 : (limit > 512) ? 512 : limit;
+  limit = limit < 1 ? 1 : limit > 512 ? 512 : limit;
 
   startCopy(currentPath);
-  
+
   function startCopy(source) {
     started++;
     if (filter) {
@@ -38,8 +38,7 @@ function ncp (source, dest, options, callback) {
         if (!filter.test(source)) {
           return cb(true);
         }
-      }
-      else if (typeof filter === 'function') {
+      } else if (typeof filter === "function") {
         if (!filter(source)) {
           return cb(true);
         }
@@ -70,11 +69,9 @@ function ncp (source, dest, options, callback) {
 
       if (stats.isDirectory()) {
         return onDir(item);
-      }
-      else if (stats.isFile()) {
+      } else if (stats.isFile()) {
         return onFile(item);
-      }
-      else if (stats.isSymbolicLink()) {
+      } else if (stats.isSymbolicLink()) {
         // Symlinks don't really need to know about the mode.
         return onLink(source);
       }
@@ -83,28 +80,27 @@ function ncp (source, dest, options, callback) {
 
   function onFile(file) {
     var target = file.name.replace(currentPath, targetPath);
-    if(rename) {
-      target =  rename(target);
+    if (rename) {
+      target = rename(target);
     }
     isWritable(target, function (writable) {
       if (writable) {
         return copyFile(file, target);
       }
-      if(clobber) {
+      if (clobber) {
         rmFile(target, function () {
           copyFile(file, target);
         });
       }
       if (modified) {
         var stat = dereference ? fs.stat : fs.lstat;
-        stat(target, function(err, stats) {
-            //if souce modified time greater to target modified time copy file
-            if (file.mtime.getTime()>stats.mtime.getTime())
-                copyFile(file, target);
-            else return cb();
+        stat(target, function (err, stats) {
+          //if souce modified time greater to target modified time copy file
+          if (file.mtime.getTime() > stats.mtime.getTime())
+            copyFile(file, target);
+          else return cb();
         });
-      }
-      else {
+      } else {
         return cb();
       }
     });
@@ -112,25 +108,24 @@ function ncp (source, dest, options, callback) {
 
   function copyFile(file, target) {
     var readStream = fs.createReadStream(file.name),
-        writeStream = fs.createWriteStream(target, { mode: file.mode });
-    
-    readStream.on('error', onError);
-    writeStream.on('error', onError);
-    
-    if(transform) {
+      writeStream = fs.createWriteStream(target, { mode: file.mode });
+
+    readStream.on("error", onError);
+    writeStream.on("error", onError);
+
+    if (transform) {
       transform(readStream, writeStream, file);
     } else {
-      writeStream.on('open', function() {
+      writeStream.on("open", function () {
         readStream.pipe(writeStream);
       });
     }
-    writeStream.once('finish', function() {
-        if (modified) {
-            //target file modified date sync.
-            fs.utimesSync(target, file.atime, file.mtime);
-            cb();
-        }
-        else cb();
+    writeStream.once("finish", function () {
+      if (modified) {
+        //target file modified date sync.
+        fs.utimesSync(target, file.atime, file.mtime);
+        cb();
+      } else cb();
     });
   }
 
@@ -221,7 +216,7 @@ function ncp (source, dest, options, callback) {
   function isWritable(path, done) {
     fs.lstat(path, function (err) {
       if (err) {
-        if (err.code === 'ENOENT') return done(true);
+        if (err.code === "ENOENT") return done(true);
         return done(false);
       }
       return done(false);
@@ -231,18 +226,15 @@ function ncp (source, dest, options, callback) {
   function onError(err) {
     if (options.stopOnError) {
       return cback(err);
-    }
-    else if (!errs && options.errs) {
+    } else if (!errs && options.errs) {
       errs = fs.createWriteStream(options.errs);
-    }
-    else if (!errs) {
+    } else if (!errs) {
       errs = [];
     }
-    if (typeof errs.write === 'undefined') {
+    if (typeof errs.write === "undefined") {
       errs.push(err);
-    }
-    else { 
-      errs.write(err.stack + '\n\n');
+    } else {
+      errs.write(err.stack + "\n\n");
     }
     return cb();
   }
@@ -250,8 +242,8 @@ function ncp (source, dest, options, callback) {
   function cb(skipped) {
     if (!skipped) running--;
     finished++;
-    if ((started === finished) && (running === 0)) {
-      if (cback !== undefined ) {
+    if (started === finished && running === 0) {
+      if (cback !== undefined) {
         return errs ? cback(errs) : cback(null);
       }
     }

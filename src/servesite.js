7edb0b5b34f473c3e59258bb434227c4584c177d
wgyt <github@wgyt.tk>
2021-03-15 15:03:45 -0400

    Update servesite.js

--- src/servesite.js
+++ src/servesite.js
@@ -2,8 +2,8 @@ exports.main = function (host, res, req, error, modules, version, ejs, dirname) 
   if (modules.website.includes(host) === true) { // if the site is in modulesjson 
     module = modules.websiteData[host].module;
     config = modules.websiteData[host].config;
-    Sitemodule = require(`${dirname}/modules/${module}/index.js`);
-    Sitemodule(host, res, req, error, version, ejs, config); // serve the site
+    sitemodule = require(`${dirname}/modules/${module}/index.js`);
+    sitemodule(host, res, req, error, version, ejs, config); // serve the site
   } else { // otherwise throw error
     error(host, res, req, 501, "site doesn't exist :(", version, ejs, modules.errorHandler.config);
   }

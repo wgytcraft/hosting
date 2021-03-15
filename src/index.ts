124d17b4f97a652677d38a593ca0fb272fa5eb87
wgyt <github@wgyt.tk>
2021-03-15 16:01:18 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,15 @@
 exports.main = function (modules: any, dirname: string, port: number) {
-  const importmodules = require("./importmodules.js").main; // this is the function that imports the modules
-  const servesite = require("./servesite.js").main; // this is the function that uses the modules to serve the site
-  const http = require("http"); // http server
-  const ejs = require("./ejs/ejs.js"); // templating engine
-  const clone = require("./git-clone/index.js"); // clone from git sources
-  const version = "1.1.0"; // verison number
+  var importmodules = require("../src/importmodules.js").main; // this is the function that imports the modules
+  var servesite = require("../src/servesite.js").main; // this is the function that uses the modules to serve the site
+  var http = require("http"); // http server
+  var ejs = require("./ejs/ejs.js"); // templating engine
+  var clone = require("./git-clone/index.js"); // clone from git sources
+  var version = "1.1.0"; // verison number
   importmodules(modules, dirname, clone); // import the modules
-  const error = require(dirname +
-    "/modules/" +
-    modules.errorHandler.module +
-    "/index.js"); // get the error handler
-  http
-    .createServer(function (req, res) {
-      const host = req.headers.host; // this is the host
+  var error = require(`${dirname}/modules/${modules.errorHandler.module}/index.js`); // get the error handler
+  http.createServer(function (req, res) {
+      var host = req.headers.host; // this is the host
       res.setHeader("X-Powered-By", "nodejs@wgytcraft/hosting");
       servesite(host, res, req, error, modules, version, ejs, dirname); // serve the site
-    })
-    .listen(port);
+    }).listen(port);
 };

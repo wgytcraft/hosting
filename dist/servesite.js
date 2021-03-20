c2ec3391aa702f25a173fbe08c46aec3335c4809
wgyt <wgyt@users.noreply.github.com>
2021-03-20 11:56:49 +0000

    style: format code

--- dist/servesite.js
+++ dist/servesite.js
@@ -1,16 +1,33 @@
-module.exports = function (host, res, req, error, modules, version, ejs, dirname) {
-    if (modules.website.includes(host) === true) {
-        // if the site is in modulesjson
-        var module = modules.websiteData[host].module;
-        var config = modules.websiteData[host].config;
-        var globalConfig = modules.globalConfig;
-        var ultimateConfig = Object.assign(globalConfig, config);
-        var sitemodule = require(dirname + "/modules/" + module + "/index.js");
-        sitemodule(host, res, req, error, version, ejs, ultimateConfig); // serve the site
-    }
-    else {
-        // otherwise throw error
-        error(host, res, req, 501, "site doesn't exist :(", version, ejs, modules.errorHandler.config);
-    }
+module.exports = function (
+  host,
+  res,
+  req,
+  error,
+  modules,
+  version,
+  ejs,
+  dirname
+) {
+  if (modules.website.includes(host) === true) {
+    // if the site is in modulesjson
+    var module = modules.websiteData[host].module;
+    var config = modules.websiteData[host].config;
+    var globalConfig = modules.globalConfig;
+    var ultimateConfig = Object.assign(globalConfig, config);
+    var sitemodule = require(dirname + "/modules/" + module + "/index.js");
+    sitemodule(host, res, req, error, version, ejs, ultimateConfig); // serve the site
+  } else {
+    // otherwise throw error
+    error(
+      host,
+      res,
+      req,
+      501,
+      "site doesn't exist :(",
+      version,
+      ejs,
+      modules.errorHandler.config
+    );
+  }
 };
 //# sourceMappingURL=servesite.js.map
